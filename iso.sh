759f4ba56ac2ecc7227836b9366bcccee14bc8b8
Bruno <brunocarvalho107@gmail.com>
2024-03-29 12:38:32 -0300

    feat: improving project build structure

--- iso.sh
+++ iso.sh
@@ -5,6 +5,6 @@ set -e
 
 mkdir -p build/isofiles/boot/grub
 cp grub.cfg build/isofiles/boot/grub
-cp sysroot/boot/myos.kernel build/isofiles/boot/
-grub_mkrescue -o build/os.iso build/isofiles
+cp sysroot/boot/kernel.bin build/isofiles/boot/
+grub_mkrescue -o build/tinyx.iso build/isofiles
 
